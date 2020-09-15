2e09c51e78248cde23df7006043bc52328eb351c
Cedrik <cedrik.git@tryninja.io>
2020-09-15 15:14:24 -0500

    typescript version update

--- src/models/arweaveBlock.ts
+++ src/models/arweaveBlock.ts
@@ -10,7 +10,7 @@ export interface IArweaveBlock {
   txs: string[];
   wallet_list: string;
   reward_addr: string;
-  tags: Array<{ [key: string]: string }>;
+  tags: { [key: string]: string }[];
   reward_pool: number;
   weave_size: number;
   block_size: number;
--- src/models/arweaveTransaction.ts
+++ src/models/arweaveTransaction.ts
@@ -2,7 +2,7 @@ export interface IArweaveTransaction {
   id: string;
   last_tx: string;
   owner: string;
-  tags?: Array<{ [key: string]: string }>;
+  tags?: { [key: string]: string }[];
   target?: string;
   quantity?: string;
   data?: string;
